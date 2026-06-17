9d68fd32a4b5d172dd6a687685f4c9001f9baf07
DauNgua <106428199+DauNgua@users.noreply.github.com>
2026-06-18 00:09:43 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'matrix-v1';
+const urlsToCache = [ './Luyen nao v7.html' ];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.map(key => { if (key !== CACHE_NAME) return caches.delete(key); })
+  )));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
